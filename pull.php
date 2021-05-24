9c53f5f07bd827bbd92910f3ca3551b7772cbbb7
Yadira <yhadi1996@gmail.com>
2021-05-23 20:24:59 -0500

    Primer commit desde gitKraken

--- pull.php
+++ pull.php
@@ -9,12 +9,14 @@ foreach($persona as $valor){
     echo "$valor <br>"
 }
 
+$auto = [
+    "Marca" =>"Ford",
+    "Modelo" =>"Focus",
+    "Año" =>"2017"];
+    foreach($auto as $ posicion => $valor){
+        echo "$posicion: $valor <br>";
+    }
+
 ?>
 
-$auto = [
-"Marca" =>"Ford",
-"Modelo" =>"Focus",
-"Año" =>"2017"];
-foreach($auto as $ posicion => $valor){
-    echo "$posicion: $valor <br>";
-}
+
